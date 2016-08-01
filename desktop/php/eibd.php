4306b393d7fc8406fd8a3b9077f9b8e53dd82b72
mika-nt28 <michaelhalbout@gmail.com>
2016-08-01 15:59:02 +0200

    Update eibd.php

--- desktop/php/eibd.php
+++ desktop/php/eibd.php
@@ -183,23 +183,21 @@ $eqLogics = eqLogic::byType('eibd');
 		</fieldset> 
 	</form>
         <legend>{{Commandes}}</legend>
-        <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande KNX}}</a>
+        	<a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande KNX}}</a>
 		<a class="btn btn-danger btn-sm eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
 		<a class="btn btn-success btn-sm eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-			<div class="form-group pull-right" style="display:inline">
-				<select class="Template form-control" data-l1key="type">
-					<option value="">{{Aucun}}</option>
-					<?php
-					foreach (eibd::devicesParameters() as $id => $info) {
-						echo '<option value="' . $id . '">' . $info['name'] . '</option>';
-					}
-					?>
-				</select>
-				<sup>
-					<i class="fa fa-question-circle tooltips" title="Choisissez si vous le souhaité un model de configuration d'équipement" style="font-size : 1em;color:grey;"></i>
-				</sup>
-				<a class="btn btn-success btn-sm Template" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter les Commande}}</a>
-		   	</div>
+		<select class="Template form-control pull-right btn-sm" data-l1key="type">
+			<option value="">{{Aucun}}</option>
+			<?php
+			foreach (eibd::devicesParameters() as $id => $info) {
+				echo '<option value="' . $id . '">' . $info['name'] . '</option>';
+			}
+			?>
+		</select>
+		<sup>
+			<i class="fa fa-question-circle tooltips" title="Choisissez si vous le souhaité un model de configuration d'équipement" style="font-size : 1em;color:grey;"></i>
+		</sup>
+		<a class="btn btn-success btn-sm Template pull-right" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter les Commande}}</a>
         <table id="table_cmd" class="table table-bordered table-condensed">
             <thead>
                 <tr>
