53de385fe92da84735b42425923c2289ca15b67a
mika-nt28 <michaelhalbout@gmail.com>
2016-08-29 11:36:49 +0200

    Update eibd.php

--- desktop/php/eibd.php
+++ desktop/php/eibd.php
@@ -36,13 +36,13 @@ $eqLogics = eqLogic::byType('eibd');
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
-					<i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
+					<i class="fa fa-plus-circle" style="font-size : 5em;color:#0049A7;"></i>
 				</center>
 				<span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{Ajouter}}</center></span>
 			</div>
 			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="height: 120px; margin-bottom: 10px; padding: 5px; border-radius: 2px; width: 160px; margin-left: 10px; position: absolute; left: 170px; top: 0px; background-color: rgb(255, 255, 255);">
 				<center>
-			      		<i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
+			      		<i class="fa fa-wrench" style="font-size : 5em;color:#0049A7;"></i>
 			    	</center>
 			    	<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>Configuration</center></span>
 			</div>
