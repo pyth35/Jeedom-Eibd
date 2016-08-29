8ed16bce0d1fe558a17ac90b597833db55ce4caf
mika-nt28 <michaelhalbout@gmail.com>
2016-08-29 14:38:13 +0200

    Update dpt.class.php

--- core/class/dpt.class.php
+++ core/class/dpt.class.php
@@ -577,13 +577,11 @@ class Dpt{
 		return '#'.$color;
 	}
 	private function strToHex($string){
-		$hex = '';
-		for ($i=0; $i<strlen($string); $i++){
-			$ord = ord($string[$i]);
-			$hexCode = dechex($ord);
-			$hex .= substr('0'.$hexCode, -2);
+		$hex='';
+		for ($i=0; $i < strlen($string); $i++){
+			$hex .= dechex(ord($string[$i]));
 		}
-		return strToUpper($hex);
+		return $hex;
 	}
 	private function hexToStr($hex){
 		$string='';
