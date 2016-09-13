5437b1d9061d685d461f7372c8adde54dc95d758
Halbout Michael <michaelhalbout@gmail.com>
2016-09-13 08:55:53 +0200

    Update install-knxd.sh

--- ressources/install-knxd.sh
+++ ressources/install-knxd.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+remove_eibd(){
+
+echo "*****************************************************************************************************"
+echo "*                              Del PTHSEM V2.0.8 libraries                                   *"
+echo "*****************************************************************************************************"
+sudo rm /etc/eibd/pthsem_VERSION
+echo $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="/usr/local/lib"
+sudo ldconfig 
+rm $TEMP_DIR/pthsem-2.0.8
+
+echo "*****************************************************************************************************"
+echo "*                              del BCUSDK V0.0.5 libraries                                   *"
+echo "*****************************************************************************************************"
+
+rm $TEMP_DIR/bcusdk-0.0.5 
+rm /etc/eibd/bcusdk_VERSION
+
+echo "*****************************************************************************************************"
+echo "*                                 Del eibd startup script                                      *"
+echo "*****************************************************************************************************"
+rm /etc/logrotate.d/eibd
+rm /etc/default/eibd
+rm /var/log/eibd.log
+rm -R /etc/eibd
+rm -rf /usr/local/lib/libeibclient.so
+rm -rf /usr/local/lib/libeibclient.so.0
+rm -rf /usr/local/lib/libeibclient.a
+rm -rf /usr/local/lib/libeibclient.la
+rm -rf /usr/local/lib/libeibclient.so.0.0.0
+}
 install_dependances ()
 {
   echo "-------------------------------------------------------------------"
@@ -105,7 +136,9 @@ else
   echo "KNXD deja installe : $KNXD_VERSION "
 fi
 echo "-------------------------------------------------------------------"
+echo "v0.10" > /etc/eibd/knxd_VERSION
 }
 
+remove_eibd
 install_dependances
 install_knxd
