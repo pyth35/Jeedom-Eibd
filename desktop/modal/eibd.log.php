a2b0b1d12f609ef4d14786e94844a253764e2851
Halbout Michael <michaelhalbout@gmail.com>
2016-11-09 13:26:41 +0100

    Create eibd.log.php

---
+++ desktop/modal/eibd.log.php
@@ -0,0 +1,31 @@
+<?php
+if (!isConnect()) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
+
+?>
+<div class='Log' style="width: 100%;height: 75%;  overflow: auto;"></div>
+<script>
+getLog();
+function getLog(){
+	$.ajax({
+		type: "POST",
+		timeout:8000, 
+		url: "plugins/openalpr/core/ajax/eibd.ajax.php",
+		data: {
+			action: "getLog",
+		},
+		dataType: 'json',
+		error: function(request, status, error) {
+			handleAjaxError(request, status, error);
+		},
+		success: function(data) { 
+			if (data.state != 'ok') {
+				$('#div_alert').showAlert({message: data.result, level: 'danger'});
+				return;
+			}
+			$('.Log').html(data.result);
+		}
+	});	
+}
+</script>
