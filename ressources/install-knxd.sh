f224afe7fd9749d7c79a809f51a968a9f53d0b16
Halbout Michael <michaelhalbout@gmail.com>
2016-11-09 09:14:14 +0100

    Ajout de l'indicateur de progression

--- ressources/install-knxd.sh
+++ ressources/install-knxd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+touch /tmp/compilation_eibd_in_progress
+echo 0 > /tmp/compilation_eibd_in_progress
 if [ -f "/etc/eibd/pthsem_VERSION" ]
 then
   echo "*****************************************************************************************************"
@@ -9,6 +11,7 @@ then
   export LD_LIBRARY_PATH="/usr/local/lib"
   sudo ldconfig 
 fi
+echo 5 > /tmp/compilation_eibd_in_progress
 if [ -f "/etc/eibd/bcusdk_VERSION" ]
 then
   echo "*****************************************************************************************************"
@@ -17,6 +20,7 @@ then
   rm bcusdk-0.0.5 
   rm /etc/eibd/bcusdk_VERSION
 fi
+echo 10 > /tmp/compilation_eibd_in_progress
 echo "*****************************************************************************************************"
 echo "*                                         Remove eibd                                               *"
 echo "*****************************************************************************************************"
@@ -56,11 +60,13 @@ if [ -f "/usr/local/lib/libeibclient.so.0.0.0" ]
 then
   rm -rf /usr/local/lib/libeibclient.so.0.0.0
 fi
+echo 15 > /tmp/compilation_eibd_in_progress
 echo "*****************************************************************************************************"
 echo "*                                         Remove knxd                                               *"
 echo "*****************************************************************************************************"
 sudo apt-get autoremove --yes -y -qq knxd
 sudo rm /var/log/knxd.log
+echo 20 > /tmp/compilation_eibd_in_progress
 echo "*****************************************************************************************************"
 echo "*                                Installation des dependances                                       *"
 echo "*****************************************************************************************************"
@@ -76,26 +82,33 @@ sudo apt-get install --yes -y -qq libtool
 sudo apt-get install --yes -y -qq libusb-1.0-0-dev 
 sudo apt-get install --yes -y -qq libsystemd-daemon-dev 
 sudo apt-get install --yes -y -qq dh-systemd
+echo 30 > /tmp/compilation_eibd_in_progress
 echo "*****************************************************************************************************"
 echo "*                        Installation de PTHSEM V2.0.8 libraries                                    *"
 echo "*****************************************************************************************************"
 sudo apt-get install cdbs --yes -y -qq
 wget https://sourceforge.net/projects/bcusdk/files/pthsem/pthsem_2.0.8.tar.gz/download
 tar xzf pthsem_2.0.8.tar.gz
+echo 35 > /tmp/compilation_eibd_in_progress
 cd pthsem-2.0.8
 sudo dpkg-buildpackage -b -uc
+echo 40 > /tmp/compilation_eibd_in_progress
 cd ..
 sudo dpkg -i libpthsem*.deb
 echo "v2.0.8" > /etc/eibd/pthsem_VERSION
+echo 50 > /tmp/compilation_eibd_in_progress
 echo "*****************************************************************************************************"
 echo "*                                      Installation de KnxD                                         *"
 echo "*****************************************************************************************************"
 git clone https://github.com/knxd/knxd.git
+echo 55 > /tmp/compilation_eibd_in_progress
 sudo mv knxd-master knxd
 cd knxd
 sudo dpkg-buildpackage -b -uc
+echo 70 > /tmp/compilation_eibd_in_progress
 cd ..
 sudo dpkg -i knxd_*.deb knxd-tools_*.deb
+echo 90 > /tmp/compilation_eibd_in_progress
 echo " " > /var/log/knxd.log
 sudo chmod 777 /var/log/knxd.log
 echo "v0.10"" > /etc/eibd/knxd_VERSION
