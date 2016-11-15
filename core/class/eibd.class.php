89093d7b710dfedec474bd481e5e3a158d1fb8e2
Halbout Michael <michaelhalbout@gmail.com>
2016-11-15 12:21:19 +0100

    Update eibd.class.php

--- core/class/eibd.class.php
+++ core/class/eibd.class.php
@@ -658,7 +658,7 @@ class eibd extends eqLogic {
 		log::remove('eibd');
 		self::deamon_stop();
 		if(file_exists('/etc/eibd/knxd_VERSION'))
-			$cmd = 'sudo knxd -u /tmp/eib -u /var/run/knx -i --eibaddr='.config::byKey('EibdGad', 'eibd').' -b';
+			$cmd = 'sudo knxd -u /tmp/eib -u /var/run/knx -i -b';
 		//$cmd = 'sudo knxd --daemon=/var/log/knx.log --pid-file=/var/run/knx.pid -D -S --Name=Jeedom_KNX --listen-tcp='.config::byKey('EibdPort', 'eibd').' --eibaddr='.config::byKey('EibdGad', 'eibd').' -b';
 		else
 			$cmd = 'sudo eibd --daemon=/var/log/knx.log --pid-file=/var/run/knx.pid -D -S -T --listen-tcp='.config::byKey('EibdPort', 'eibd').' --eibaddr='.config::byKey('EibdGad', 'eibd');
@@ -707,7 +707,7 @@ class eibd extends eqLogic {
 			$cmd='sudo pkill knxd';
 		else
 			$cmd='sudo pkill eibd';
-		$cmd .= ' >> ' . log::getPathToLog('eibd_update') . ' 2>&1 &';
+		$cmd .= ' >> ' . log::getPathToLog('eibd') . ' 2>&1 &';
 		exec($cmd);
 		$cron = cron::byClassAndFunction('eibd', 'BusMonitor');
 		if (is_object($cron)) {
