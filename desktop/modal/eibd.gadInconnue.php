2f0adda540a6bc97bc1df5350e1e18f27451bff2
Halbout Michael <michaelhalbout@gmail.com>
2017-03-30 12:47:19 +0200

    Update eibd.gadInconnue.php

--- desktop/modal/eibd.gadInconnue.php
+++ desktop/modal/eibd.gadInconnue.php
@@ -41,12 +41,8 @@ function getKnxGadInconue () {
 				return;
 			}
 			$('#table_GadInconue tbody').html('');
-			//alert(data.result);
-			var monitors=jQuery.parseJSON(data.result);
-			jQuery.each(monitors.reverse(),function(key, value) {
-				//"{"Mode":"Write","AdresseGroupe":"7\/2\/0","AdressePhysique":"1.0.7","data":"0x 0","valeur":0,"dpt":"1.xxx"}"
-
-			  $('#table_GadInconue tbody').append($("<tr>")
+			jQuery.each(jQuery.parseJSON(data.result),function(key, value) {
+				$('#table_GadInconue tbody').append($("<tr>")
 					.append($("<td>").text(value.monitor.AdressePhysique))
 					.append($("<td>").text(value.monitor.AdresseGroupe))
 					.append($("<td>").text(value.monitor.dpt))
