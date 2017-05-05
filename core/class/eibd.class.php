ead8eb3d2b164892c289f7493180c24cbd8067c6
Halbout Michael <michaelhalbout@gmail.com>
2017-05-05 13:49:38 +0200

    Update eibd.class.php

--- core/class/eibd.class.php
+++ core/class/eibd.class.php
@@ -381,16 +381,16 @@ class eibd extends eqLogic {
 		log::add('eibd','debug','Lancement du scripte de mise a  jours des Flags');
 		foreach(eqLogic::byType('eibd') as $eqLogic){
 			foreach($eqLogic->getCmd() as $cmd){
-				if(isset($cmd->getConfiguration('eventOnly') && $cmd->getConfiguration('eventOnly')){
+				if(isset($cmd->getConfiguration('eventOnly')) && $cmd->getConfiguration('eventOnly')){
 					log::add('eibd','debug','Remplacement du Flags eventOnly  par FlagWrite et FlagUpdate sur la commande '.$cmd->getHumaneName());
 					$cmd->setConfiguration('FlagWrite',true);
 					$cmd->setConfiguration('FlagUpdate',true);
 				}
-				if(isset($cmd->getConfiguration('init') && $cmd->getConfiguration('init')){
+				if(isset($cmd->getConfiguration('init')) && $cmd->getConfiguration('init')){
 					log::add('eibd','debug','Remplacement du Flags init  par FlagInit sur la commande '.$cmd->getHumaneName());
 					$cmd->setConfiguration('FlagInit',true);
 				}
-				if(isset($cmd->getConfiguration('transmitReponse') && $cmd->getConfiguration('transmitReponse')){
+				if(isset($cmd->getConfiguration('transmitReponse')) && $cmd->getConfiguration('transmitReponse')){
 					log::add('eibd','debug','Remplacement du Flags transmitReponse par FlagRead sur la commande '.$cmd->getHumaneName());
 					$cmd->setConfiguration('FlagRead',true);
 					$cmd->setValue($cmd->getConfiguration('ObjetTransmit'));
