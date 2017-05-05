d285fe706eb66380a29ede0810ac87b3a3801c8c
Halbout Michael <michaelhalbout@gmail.com>
2017-05-05 11:06:51 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -2,14 +2,14 @@
 function eibd_update() {
 	foreach(eqLogic::byType('eibd') as $eqLogic){
 		foreach($eqLogic->getCmd() as $cmd){
-			if(isset($cmd->getConfiguration('eventOnly')){
+			if(isset($cmd->getConfiguration('eventOnly') && $cmd->getConfiguration('eventOnly')){
 				$cmd->setConfiguration('FlagWrite',true);
 				$cmd->setConfiguration('FlagUpdate',true);
 			}
-			if(isset($cmd->getConfiguration('init')){
+			if(isset($cmd->getConfiguration('init') && $cmd->getConfiguration('init')){
 				$cmd->setConfiguration('FlagInit',true);
 			}
-			if(isset($cmd->getConfiguration('transmitReponse')){
+			if(isset($cmd->getConfiguration('transmitReponse') && $cmd->getConfiguration('transmitReponse')){
 				$cmd->setConfiguration('FlagRead',true);
 				$cmd->setValue($cmd->getConfiguration('ObjetTransmit'));
 			}
