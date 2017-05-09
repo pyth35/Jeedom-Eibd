ecd6ecad43892db3e35c72fa5eb922a6c595ecd4
Halbout Michael <michaelhalbout@gmail.com>
2017-05-09 08:53:21 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -8,21 +8,21 @@ function eibd_update() {
 	foreach(eqLogic::byType('eibd') as $eqLogic){ 
 		foreach($eqLogic->getCmd('info') as $cmd){ 
 			if(is_object($cmd)){
-			if($cmd->getConfiguration('FlagWrite','NoExist') == 'NoExist' && $cmd->getConfiguration('FlagUpdate','NoExist') == 'NoExist'){ 
-				log::add('eibd','debug','Remplacement du Flags eventOnly  par FlagWrite et FlagUpdate sur la commande '.$cmd->getHumanName()); 
-				$cmd->setConfiguration('FlagWrite',$cmd->getConfiguration('eventOnly')); 
-				$cmd->setConfiguration('FlagUpdate',$cmd->getConfiguration('eventOnly')); 
-			} 
-			if($cmd->getConfiguration('FlagInit','NoExist') == 'NoExist'){ 
-				log::add('eibd','debug','Remplacement du Flags init  par FlagInit sur la commande '.$cmd->getHumanName()); 
-				$cmd->setConfiguration('FlagInit',$cmd->getConfiguration('init')); 
-			} 
-			if($cmd->getConfiguration('FlagRead','NoExist') == 'NoExist'){ 
-				log::add('eibd','debug','Remplacement du Flags transmitReponse par FlagRead sur la commande '.$cmd->getHumanName()); 
-				$cmd->setConfiguration('FlagRead',$cmd->getConfiguration('transmitReponse')); 
-				$cmd->setValue($cmd->getConfiguration('ObjetTransmit')); 
-			} 
-			//$cmd->save(); 
+				if($cmd->getConfiguration('FlagWrite','NoExist') == 'NoExist' && $cmd->getConfiguration('FlagUpdate','NoExist') == 'NoExist'){ 
+					log::add('eibd','debug','Remplacement du Flags eventOnly  par FlagWrite et FlagUpdate sur la commande '.$cmd->getHumanName()); 
+					$cmd->setConfiguration('FlagWrite',$cmd->getConfiguration('eventOnly')); 
+					$cmd->setConfiguration('FlagUpdate',$cmd->getConfiguration('eventOnly')); 
+				} 
+				if($cmd->getConfiguration('FlagInit','NoExist') == 'NoExist'){ 
+					log::add('eibd','debug','Remplacement du Flags init  par FlagInit sur la commande '.$cmd->getHumanName()); 
+					$cmd->setConfiguration('FlagInit',$cmd->getConfiguration('init')); 
+				} 
+				if($cmd->getConfiguration('FlagRead','NoExist') == 'NoExist'){ 
+					log::add('eibd','debug','Remplacement du Flags transmitReponse par FlagRead sur la commande '.$cmd->getHumanName()); 
+					$cmd->setConfiguration('FlagRead',$cmd->getConfiguration('transmitReponse')); 
+					$cmd->setValue($cmd->getConfiguration('ObjetTransmit')); 
+				} 
+				$cmd->save(); 
 			}
 		}
 	}
