73130649372b8ae75ccd591650760dd30caee369
Halbout Michael <michaelhalbout@gmail.com>
2017-05-09 08:37:05 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -9,19 +9,19 @@ function eibd_update() {
 		foreach($eqLogic->getCmd() as $cmd){ 
 			if(!isset($cmd->getConfiguration('FlagWrite')) && !isset($cmd->getConfiguration('FlagUpdate')) && isset($cmd->getConfiguration('eventOnly'))){ 
 				log::add('eibd','debug','Remplacement du Flags eventOnly  par FlagWrite et FlagUpdate sur la commande '.$cmd->getHumanName()); 
-				$cmd->setConfiguration('FlagWrite',true); 
-				$cmd->setConfiguration('FlagUpdate',true); 
+				//$cmd->setConfiguration('FlagWrite',true); 
+				//$cmd->setConfiguration('FlagUpdate',true); 
 			} 
 			if(!isset($cmd->getConfiguration('FlagInit')) && isset($cmd->getConfiguration('init'))){ 
 				log::add('eibd','debug','Remplacement du Flags init  par FlagInit sur la commande '.$cmd->getHumanName()); 
-				$cmd->setConfiguration('FlagInit',true); 
+				//$cmd->setConfiguration('FlagInit',true); 
 			} 
 			if(!isset($cmd->getConfiguration('FlagRead')) && isset($cmd->getConfiguration('transmitReponse'))){ 
 				log::add('eibd','debug','Remplacement du Flags transmitReponse par FlagRead sur la commande '.$cmd->getHumanName()); 
-				$cmd->setConfiguration('FlagRead',true); 
-				$cmd->setValue($cmd->getConfiguration('ObjetTransmit')); 
+				//$cmd->setConfiguration('FlagRead',true); 
+				//$cmd->setValue($cmd->getConfiguration('ObjetTransmit')); 
 			} 
-			$cmd->save(); 
+			//$cmd->save(); 
 		}
 	}
 }
