90c2a3d22e81e2c62dba6007700f33f6b8cf51e7
Halbout Michael <michaelhalbout@gmail.com>
2017-05-09 08:41:05 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -7,7 +7,7 @@ function eibd_update() {
 	log::add('eibd','debug','Lancement du scripte de mise a  jours des Flags'); 
 	foreach(eqLogic::byType('eibd') as $eqLogic){ 
 		foreach($eqLogic->getCmd('info') as $cmd){ 
-			if(!isset($cmd->getConfiguration('FlagWrite')) && !isset($cmd->getConfiguration('FlagUpdate')) && isset($cmd->getConfiguration('eventOnly'))){ 
+			if(!isset($cmd->getConfiguration('FlagWrite')) /*&& !isset($cmd->getConfiguration('FlagUpdate')) && isset($cmd->getConfiguration('eventOnly'))*/){ 
 				//log::add('eibd','debug','Remplacement du Flags eventOnly  par FlagWrite et FlagUpdate sur la commande '.$cmd->getHumanName()); 
 				//$cmd->setConfiguration('FlagWrite',true); 
 				//$cmd->setConfiguration('FlagUpdate',true); 
