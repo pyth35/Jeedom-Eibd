ff7945c9379b323599f2577143526df20b1fc04a
Halbout Michael <michaelhalbout@gmail.com>
2017-07-27 16:02:29 +0200

    Update eibd.busmoniteur.php

--- desktop/modal/eibd.busmoniteur.php
+++ desktop/modal/eibd.busmoniteur.php
@@ -33,50 +33,6 @@ $('body').on('eibd::monitor', function (_event,_options) {
 		.append($("<td>").text(monitors.DataPointType))
 		.append($("<td>").text(monitors.valeur)));			
 	$('#table_BusMonitor').trigger('update');
-});
-//event::add('clientSIP::call', utils::o2a($monitor));
-/*getKnxBusMonitor();
-function getKnxBusMonitor () {
-	$.ajax({
-		type: 'POST',
-	async: false,
-	url: 'plugins/eibd/core/ajax/eibd.ajax.php',
-		data: {
-			action: 'getCacheMonitor',
-		},
-		dataType: 'json',
-		global: false,
-		error: function(request, status, error) {
-			setTimeout(function() {
-				getKnxBusMonitor()
-			}, 100);
-		},
-		success: function(data) {
-			if (data.state != 'ok') {
-				$('#div_alert').showAlert({message: data.result, level: 'danger'});
-				return;
-			}
-			$('#table_BusMonitor tbody').html('');
-			//alert(data.result);
-			var monitors=jQuery.parseJSON(data.result);
-			jQuery.each(monitors.reverse(),function(key, value) {
-			  $('#table_BusMonitor tbody').append($("<tr>")
-					.append($("<td>").text(value.datetime))
-					.append($("<td>").text(value.monitor.Mode))
-					.append($("<td>").text(value.monitor.AdressePhysique))
-					.append($("<td>").text(value.monitor.AdresseGroupe))
-					.append($("<td>").text(value.monitor.data))
-					.append($("<td>").text(value.monitor.DataPointType))
-					.append($("<td>").text(value.monitor.valeur)));
-			});				
-			$('#table_BusMonitor').trigger('update');
-			if ($('#md_modal').dialog('isOpen') === true) {
-				setTimeout(function() {
-					getKnxBusMonitor()
-				}, 100);
-			}
-		}
-	});
-}*/		   
+});	   
 </script>
 		
